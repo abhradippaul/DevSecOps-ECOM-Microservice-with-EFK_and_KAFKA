5b165f59f0caae6eba214917606a38b7ee5347e0
Abhradip Paul <abhradipserampore@gmail.com>
2025-12-01 22:25:57 +0530

    Change some structured

---
+++ Project-Code/product-service/src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(
+  cors({
+    origin: ["http://localhost:3002", "http://localhost:3003"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  return res.status(200).json({
+    status: "ok",
+    uptime: process.uptime(),
+    timestamp: Date.now(),
+  });
+});
+
+app.listen(8000, () => {
+  console.log("Product service is running on 8000");
+});
